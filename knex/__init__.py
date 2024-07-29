836ce6d441568fa2f1167a7c8465893d191ffb05
Gaétan Muck <gaetan.muck@gmail.com>
2024-07-29 15:16:20 +0200

    update prompt

--- knex/__init__.py
+++ knex/__init__.py
@@ -1,4 +1,5 @@
 from .main import extract, generate_visual, KnexOptions, KnexReturn
+from .assertions import get_assertions
 from .globals import nlp
 from .schema import *
 from .constants import ontology, classes, properties
--- knex/constants/prompt.py
+++ knex/constants/prompt.py
@@ -1,16 +1,38 @@
 prompt = """
-Provide me all statements you understand from the following text. 
-Be exhaustive and explicit. 
-Never use pronouns.
+Provide me with all atomic assertions from the following text, and add the assertion main topic as described in the example.
+Be explicit on the names (do not use pronouns like 'he', 'his', 'her', ...).
+Give me as much assertions as possible.
+Do not repeat yourself, in any kind of manner.
 Dates should have the following format: day.month.year (eg 31.12.2000).
-Your answer should be a list of short phrases about a single fact.
+Do not be verbose, here is what your answer should look like:
+"Atomic assertions:
+- John Doe was French -> nationality
+- John Doe was a carpenter -> job
+- John Doe was the son of Martin -> family
+- John Doe married Augustine in 1988 -> family
+- Augustine was a Catholic -> religion
+- Augustine's parents are Albert and Justine -> family
+- Albert was a general -> job"
 
-Examples:
-- John Doe is the son of Martin
-- John Doe married Augustine in 1988
-- Augustine was a Catholic
-- Augustine's parents are Albert and Justine
-- Albert was a general
-                 
-Text: "[INPUT_TEXT]"
+Text: "[INPUT_TEXT]"  
 """
+
+
+
+# Old prompt from v0, used with mistral
+# prompt = """
+# Provide me all statements you understand from the following text. 
+# Be exhaustive and explicit. 
+# Never use pronouns.
+# Dates should have the following format: day.month.year (eg 31.12.2000).
+# Your answer should be a list of short phrases about a single fact.
+
+# Examples:
+# - John Doe is the son of Martin
+# - John Doe married Augustine in 1988
+# - Augustine was a Catholic
+# - Augustine's parents are Albert and Justine
+# - Albert was a general
+                 
+# Text: "[INPUT_TEXT]"
+# """
